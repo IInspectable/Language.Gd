0fa5f06238a02206fec9a48408830651687d768e
IInspectable <github@smjh.de>
2022-11-10 11:30:56 +0100

    misc

--- Build.bat
+++ Build.bat
@@ -5,7 +5,7 @@ if "%config%" == "" (
    set config=Debug
 )
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:restore
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" Language.Gd.sln /p:Configuration="%config%" /maxcpucount:4 /v:n
+"%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:restore
+"%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" Language.Gd.sln /p:Configuration="%config%" /maxcpucount:4 /v:n
 
 REM Pause
